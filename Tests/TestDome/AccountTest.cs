cc3cfc845c3462d84ae662a819aa74baf98264f2
Platfuss <michal.stanislaw.drelich@gmail.com>
2024-04-30 20:05:55 +0200

    AccountTest

---
+++ Tests/TestDome/AccountTest.cs
@@ -0,0 +1,54 @@
+using Challenges.TestDome;
+
+namespace Tests.TestDome;
+
+[TestFixture]
+public class Tests
+{
+    private double epsilon = 1e-6;
+
+    [Test]
+    public void AccountCannotDepositNegativeValue()
+    {
+        Account account = new Account(100);
+        account.Deposit(-10);
+
+        Assert.AreEqual(0, account.Balance, epsilon);
+    }
+
+    [Test]
+    public void AccountCannotWithdrawNegativeValue()
+    {
+        Account account = new Account(100);
+        account.Withdraw(-10);
+
+        Assert.AreEqual(0, account.Balance, epsilon);
+    }
+
+    [Test]
+    public void AccountCannotOverstepOverdraftLimit()
+    {
+        Account account = new Account(100);
+        account.Withdraw(150);
+
+        Assert.AreEqual(0, account.Balance, epsilon);
+    }
+
+    [Test]
+    public void AccountDepositCorrectValue()
+    {
+        Account account = new Account(100);
+        account.Deposit(10);
+
+        Assert.AreEqual(10, account.Balance, epsilon);
+    }
+
+    [Test]
+    public void AccountWithdrawCorrectValue()
+    {
+        Account account = new Account(100);
+        account.Withdraw(10);
+
+        Assert.AreEqual(-10, account.Balance, epsilon);
+    }
+}
