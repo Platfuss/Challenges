575ba636133be22364f314e08da518f3a32a71a3
Platfuss <michal.stanislaw.drelich@gmail.com>
2024-05-02 19:05:37 +0200

    SortedSearch

---
+++ TestDome/SortedSearch.cs
@@ -0,0 +1,17 @@
+﻿using Challenges.Helpers;
+using System;
+
+namespace Challenges.TestDome;
+internal class SortedSearch : IStartable
+{
+    public void Start()
+    {
+        Console.WriteLine(CountNumbers(new int[] { 1, 3, 5, 7 }, 4));
+    }
+
+    public static int CountNumbers(int[] sortedArray, int lessThan)
+    {
+        int idx = Array.BinarySearch(sortedArray, lessThan);
+        return idx < 0 ? ~idx : idx;
+    }
+}
