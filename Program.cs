4da9d3bcaa6427510b5773e657b98a787c1d8572
Platfuss <michal.stanislaw.drelich@gmail.com>
2024-05-05 22:26:49 +0200

    SortedSearch - own Search

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        IStartable startable = new DecoratorStream();
+        IStartable startable = new SortedSearch();
         Console.WriteLine($"Running: {startable.GetType().Name}");
         startable.Start();
 
--- TestDome/SortedSearch.cs
+++ TestDome/SortedSearch.cs
@@ -1,5 +1,6 @@
 ﻿using Challenges.Helpers;
 using System;
+using System.Linq;
 
 namespace Challenges.TestDome;
 /// <summary>
@@ -9,12 +10,38 @@ internal class SortedSearch : IStartable
 {
     public void Start()
     {
-        Console.WriteLine(CountNumbers(new int[] { 1, 2, 3, 4 }, 4));
+        Console.WriteLine(CountNumbers(new int[] { 1, 3, 5, 7 }, 4));
     }
 
     public static int CountNumbers(int[] sortedArray, int lessThan)
     {
-        int idx = Array.BinarySearch(sortedArray, lessThan);
-        return idx < 0 ? ~idx : idx;
+        //int idx = Array.BinarySearch(sortedArray, lessThan);
+        //return idx < 0 ? ~idx : idx;
+        return BinarySearch(sortedArray, lessThan);
+    }
+
+    private static int BinarySearch(int[] sortedArray, int lessThan)
+    {
+        if (sortedArray.First() >= lessThan)
+            return 0;
+        else if (sortedArray.Last() < lessThan)
+            return sortedArray.Length;
+
+        int leftBound = 0;
+        int rightBound = sortedArray.Length - 1;
+        int currentIdx = sortedArray.Length / 2;
+
+        while (leftBound < rightBound)
+        {
+            if (sortedArray[currentIdx] == lessThan)
+                return currentIdx;
+            else if (sortedArray[currentIdx] < lessThan)
+                leftBound = currentIdx + 1;
+            else if (sortedArray[currentIdx] > lessThan)
+                rightBound = currentIdx;
+
+            currentIdx = leftBound + (rightBound - leftBound) / 2;
+        }
+        return currentIdx;
     }
 }
